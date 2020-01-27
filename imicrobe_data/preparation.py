a8a5988eaea690e793098acdcc511f3060c4b08a
Denis <loven7doo@gmail.com>
2020-01-27 13:15:56 +0300

    fna rwading ok; problems with eagle.lib.SeqsDict.load_from_file

--- imicrobe_data/preparation.py
+++ imicrobe_data/preparation.py
@@ -42,9 +42,10 @@ def prepare_data(fna_path=FNA_PATH,
     print("18S rRNA sequences read")
     rna_names_conv = convert_rna_names(rna_seqs, samples_dict)
     print("\n")
-    fna_seqs = SeqsDict.load_from_file(fna_path, seqs_format="fasta", low_memory=True)
+    fna_names_conv = convert_fna_names(in_fna_path=fna_path, 
+                                       sample_names=rna_names_conv, 
+                                       out_fna_path=prepared_fna_path)
     print("Transcriptome sequences read")
-    fna_names_conv = convert_fna_names(fna_seqs, rna_names_conv)
 
     transcriptomes = list()
     fna_ids_conv = dict()
@@ -52,16 +53,12 @@ def prepare_data(fna_path=FNA_PATH,
         transcriptomes.append(GenomeInfo(genome_id=sample_name, 
                                          org_name=samples_dict[sample_name],
                                          fna_path=prepared_fna_path,
-                                         fna_id_list=list(fna_names_conv[sample_name].keys())).get_json())
-        fna_ids_conv.update(fna_names_conv[sample_name])
+                                         fna_id_list=list(fna_names_conv[sample_name])).get_json())
     with open(transcriptomes_path, "w") as transcriptomes_f:
-        json.dump(transcriptomes, transcriptomes_f)
+        json.dump(transcriptomes, transcriptomes_f, indent=2)
 
     rna_seqs.rename_seqs({rna_names_conv[t_name]: t_name for t_name in fna_names_conv})
     rna_seqs.get_sample(list(fna_names_conv.keys()), low_memory=False).dump(prepared_18s_path, seqs_format="fasta")
-    fna_seqs.rename_seqs({fna_ids_conv[t_name]: t_name for t_name in fna_ids_conv})
-    fna_seqs.get_sample(list(fna_ids_conv.keys()), low_memory=True).dump(prepared_fna_path, seqs_format="fasta")
-
 
 def get_samples_dict(fna_meta_path, sep):
     fna_meta_df = pd.read_csv(fna_meta_path, sep=sep)[["BIOMATERIAL_NAME", "SAMPLE_DESCRIPTION"]]
@@ -89,20 +86,54 @@ def convert_rna_names(in_rna_seqs, sample_names):
     return transformed_names
 
 
-def convert_fna_names(in_fna_seqs, sample_names):
-    transformed_names = defaultdict(dict)
-    for seq_name in in_fna_seqs:
-        transformed_name = None
-        sample_name = None
-        transformed_name_list = seq_name.split(" ")[1].split("=")[-1].split("-")
-        transformed_name = transformed_name_list[0][:10] + "-" + transformed_name_list[1]
-        sample_name = transformed_name.split("-")[0]
-        if sample_name in sample_names:
-            if transformed_name in transformed_names[sample_name]:
-                l = len(transformed_names[sample_name])
-                transformed_names[sample_name][transformed_name+"_"+str(l)] = seq_name
-            else:
-                transformed_names[sample_name][transformed_name] = seq_name
-        else:
-            print("sample '%s' is absent in sample_names" % transformed_name)
-    return transformed_names
+def convert_fna_names(in_fna_path, sample_names, out_fna_path):
+    sample_fna_ids = defaultdict(set)
+    with open(out_fna_path, "w") as out_fna_f:
+        with open(in_fna_path) as in_fna_f:
+            seq_list = list()
+            transformed_name = None
+            sample_name = None
+            for line_ in in_fna_f:
+                line = None
+                line = line_.strip()
+                if not line:
+                    continue
+                if line[0] == ">":
+                    if transformed_name is not None:
+                        if sample_name in sample_names:
+                            if transformed_name in sample_fna_ids[sample_name]:
+                                l = len(sample_fna_ids[sample_name])
+                                sample_fna_ids[sample_name].add(transformed_name+"_"+str(l))
+                                out_fna_f.write(">"+transformed_name+"_"+str(l)+"\n")
+                            else:
+                                sample_fna_ids[sample_name].add(transformed_name)
+                                out_fna_f.write(">"+transformed_name+"\n")
+                            out_fna_f.write("".join(seq_list)+"\n")
+                        else:
+                            print("sample '%s' is absent in sample_names" % transformed_name)
+                        transformed_name = None
+                        sample_name = None
+                        seq_list = list()
+                    transformed_name_list = line.split(" ")[1].split("=")[-1].split("-")
+                    transformed_name = transformed_name_list[0][:10] + "-" + transformed_name_list[1]
+                    sample_name = transformed_name.split("-")[0]
+                else:
+                    seq_list.append(line)
+                    
+            if transformed_name is not None:
+                if sample_name in sample_names:
+                    if transformed_name in sample_fna_ids[sample_name]:
+                        l = len(sample_fna_ids[sample_name])
+                        sample_fna_ids[sample_name].add(transformed_name+"_"+str(l))
+                        out_fna_f.write(">"+transformed_name+"_"+str(l)+"\n")
+                    else:
+                        sample_fna_ids[sample_name].add(transformed_name)
+                        out_fna_f.write(">"+transformed_name+"\n")
+                    out_fna_f.write("".join(seq_list)+"\n")
+                else:
+                    print("sample '%s' is absent in sample_names" % transformed_name)
+                transformed_name = None
+                sample_name = None
+                seq_list = list()
+        
+    return sample_fna_ids
